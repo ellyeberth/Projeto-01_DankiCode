a61c4dc088d4f0f387a39cc0ecc942a78f7d8774
ellyeberth <80013248+ellyeberth@users.noreply.github.com>
2022-06-20 00:29:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximus-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="./css/style.css"> 
+
+    <title>Projeto 01</title>
+</head>
+
+<body>
+    <header>
+        <img src="./img/logo.png" alt="Logo Header">
+    </header>
+
+    <section class="section-1">
+        <div>
+            <ul>
+                <li>Nulla quis dolor gravida nisi efficitur posuere.</li>
+                <li>Vestibulum sed ex bibendum augue consequat consequat.</li>
+                <li>Morbi vel leo pulvinar, efficitur nunc at, congue sem.</li>
+            </ul>
+        </div>
+
+        <form action="">
+            <h1>
+                Preencha o formulário e inicie a sua
+                caminhada para virar um webmaster!
+            </h1>
+
+            <h3>Nome*</h3>
+            <input type="text" name="nome" maxlength="100" placeholder="Digite seu nome aqui..." required>
+
+            <h3>E-mail*</h3>
+            <input type="text" name="email" maxlength="100" placeholder="E-mail" required>
+
+            <h3>Telefone*</h3>
+            <input type="text" name="telefone" maxlength="" placeholder="Telefone para contato..." required>
+
+            <h3>* Campos Obrigatórios!</h3>
+
+            <input type="submit" value="Enviar">
+        </form>
+        <div class="clear"></div>
+    </section>
+    <section class="section-2">
+        <h1>Lorem Ipsum</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at mauris tristique nisi sagittis elementum at
+            sed erat. Curabitur vitae ligula rhoncus lorem efficitur dapibus. Donec id viverra sapien. Ut a pharetra
+            tellus. Proin auctor lectus sed efficitur porttitor. Proin eget magna nec massa volutpat dignissim. Integer
+            rhoncus mollis maximus. In ac rhoncus tortor. Aenean nec placerat orci.
+        </p>
+    </section>
+    <section class="section-3">
+
+        <div class="arrow">
+            <img src="./img/arrow.png" alt="Imagem Arrow">
+        </div>
+        <h1>Aprenda com quem está no mercado!</h1>
+        <div class="icon">
+            <img src="./img/icon.png" alt="Imagem Icon">
+
+            <h1>Lorem Ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt risus a nunc interdum,
+                quis aliquam neque ultricies. Nulla ac velit ex.</p>
+        </div>
+        <div class="icon">
+            <img src="./img/icon.png" alt="Imagem Icon">
+
+            <h1>Lorem Ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt risus a nunc interdum,
+                quis aliquam neque ultricies. Nulla ac velit ex.</p>
+        </div>
+        <div class="icon">
+            <img src="./img/icon.png" alt="Imagem Icon">
+
+            <h1>Lorem Ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt risus a nunc interdum,
+                quis aliquam neque ultricies. Nulla ac velit ex.</p>
+        </div>
+    </section>
+    <footer>
+        <h3>Copyright © 2021 Ellyeberth Leandro. Todos os Direitos Reservados | Aluno Programador Full Stack</h3>
+        <h4>Juazeiro do Norte - Ceará</h4>
+    </footer>
+</body>
+
+</html>
